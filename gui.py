1fd0a5b11c4cf8121939ddff61f0c4733d052add
Fidel Serrano <serranoycandela@gmail.com>
2019-02-01 21:45:10 -0600

    Update gui.py

--- gui.py
+++ gui.py
@@ -3,7 +3,7 @@
 # Form implementation generated from reading ui file 'gui.ui',
 # licensing of 'gui.ui' applies.
 #
-# Created: Wed Jan 30 22:31:44 2019
+# Created: Fri Feb  1 21:45:00 2019
 #      by: pyside2-uic  running on PySide2 5.12.0
 #
 # WARNING! All changes made in this file will be lost!
@@ -35,12 +35,21 @@ class Ui_MainWindow(object):
         self.textBrowser.setObjectName("textBrowser")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar()
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 900, 22))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 900, 21))
         self.menubar.setObjectName("menubar")
+        self.menuOpciones = QtWidgets.QMenu(self.menubar)
+        self.menuOpciones.setObjectName("menuOpciones")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
+        self.actionAgregar_o_quitar_personas = QtWidgets.QAction(MainWindow)
+        self.actionAgregar_o_quitar_personas.setObjectName("actionAgregar_o_quitar_personas")
+        self.actionAgregar_o_quitar_proyectos = QtWidgets.QAction(MainWindow)
+        self.actionAgregar_o_quitar_proyectos.setObjectName("actionAgregar_o_quitar_proyectos")
+        self.menuOpciones.addAction(self.actionAgregar_o_quitar_personas)
+        self.menuOpciones.addAction(self.actionAgregar_o_quitar_proyectos)
+        self.menubar.addAction(self.menuOpciones.menuAction())
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -49,4 +58,7 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(QtWidgets.QApplication.translate("MainWindow", "MainWindow", None, -1))
         self.input_carpeta.setText(QtWidgets.QApplication.translate("MainWindow", "Carpeta Facturas", None, -1))
         self.label.setText(QtWidgets.QApplication.translate("MainWindow", "Proyecto", None, -1))
+        self.menuOpciones.setTitle(QtWidgets.QApplication.translate("MainWindow", "Opciones", None, -1))
+        self.actionAgregar_o_quitar_personas.setText(QtWidgets.QApplication.translate("MainWindow", "Agregar o quitar personas", None, -1))
+        self.actionAgregar_o_quitar_proyectos.setText(QtWidgets.QApplication.translate("MainWindow", "Agregar o quitar proyectos", None, -1))
 
